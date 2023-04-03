de3b79c447fd6926817c5bd9d0dcf1710a1500c6
mnapthine <mnapthine@gmail.com>
2023-04-03 10:13:06 +0100

    Add components to indexLib

--- src/indexLib.ts
+++ src/indexLib.ts
@@ -3,11 +3,17 @@
   It's what will get exposed to other packages when added as a dependency.
 */
 
-// export * from "./typings/shared-types";
+export * from "./typings/shared-types";
 
 /** Default components in alphabetical order */
-// export { default as Button } from "./components/Button/Button";
-// export * from "./components/Button/Button";
-
-// export { default as Text } from "./components/Text/Text";
-// export * from "./components/Text/Text";
+export * from "./components/PageActions/PageActions";
+export { default as Preview } from "./components/Preview/Preview";
+export * from "./components/Preview/Preview";
+export { default as PreviewActions } from "./components/PreviewActions/PreviewActions";
+export * from "./components/PreviewActions/PreviewActions";
+export { default as PreviewChrome } from "./components/PreviewChrome/PreviewChrome";
+export * from "./components/PreviewChrome/PreviewChrome";
+export { default as PreviewMetaData } from "./components/PreviewMetaData/PreviewMetaData";
+export * from "./components/PreviewMetaData/PreviewMetaData";
+export { default as PreviewModes } from "./components/PreviewModes/PreviewModes";
+export * from "./components/PreviewModes/PreviewModes";
