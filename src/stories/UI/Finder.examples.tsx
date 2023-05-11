3871c1f6abbe521734fde5ce8112cd6f9237bdc5
mnapthine <mnapthine@gmail.com>
2023-05-11 16:20:23 +0100

    Moving theme stuff over into CMS theme from Shelley theme.

--- src/stories/UI/Finder.examples.tsx
+++ src/stories/UI/Finder.examples.tsx
@@ -1,4 +1,13 @@
-import { Button, Checkbox } from "@actionishope/shelley";
+import {
+  Button,
+  Checkbox,
+  TableView,
+  Cell,
+  Column,
+  Row,
+  TableBody,
+  TableHeader,
+} from "@actionishope/shelley";
 import { Finder } from "../../components/Finder/Finder";
 import { BasicHeader } from "./Header.examples";
 import Add from "../../components/icons/Add";
@@ -36,9 +45,45 @@ export const BasicExample = () => {
           </div>
         )}
       >
-        {(isMobile: boolean) => (
-          <p>Main Content. {isMobile ? "mobileOn" : "mobileOff"}</p>
-        )}
+        <>
+          {(isMobile: boolean) => (
+            <p>Main Content. {isMobile ? "mobileOn" : "mobileOff"}</p>
+          )}
+          <TableView
+            aria-label="Example table with static contents"
+            selectionMode="multiple"
+            onRowAction={() => console.log("Opening page...")}
+            vol={1}
+          >
+            <TableHeader>
+              <Column>Name</Column>
+              <Column>Type</Column>
+              <Column align="end">Date Modified</Column>
+            </TableHeader>
+            <TableBody>
+              <Row>
+                <Cell>Games</Cell>
+                <Cell>File folder</Cell>
+                <Cell>6/7/2020</Cell>
+              </Row>
+              <Row>
+                <Cell>Program Files</Cell>
+                <Cell>File folder</Cell>
+                <Cell>4/7/2021</Cell>
+              </Row>
+              <Row>
+                <Cell>bootmgr</Cell>
+                <Cell>System file</Cell>
+                <Cell>11/20/2010</Cell>
+              </Row>
+              <Row>
+                <Cell>log.txt</Cell>
+                <Cell>Text Document</Cell>
+                <Cell>1/18/2016</Cell>
+              </Row>
+            </TableBody>
+          </TableView>
+        </>
       </Finder>
     </>
   );
