8c590323aa28776b56b1b7a28ba12612a7a86af1
Mark Napthine <mnapthine@gmail.com>
2023-08-29 10:07:33 +0100

    Fix: Portal types

--- src/components/SlateArea/components/Portal.tsx
+++ src/components/SlateArea/components/Portal.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom";
 
-export const Portal = ({ children }: any) =>
-  ReactDOM.createPortal(children, window.document.getElementById("root")!);
+export const Portal = ({ children }: Element) => {
+  const rootElement = window.document.getElementById("root");
+  rootElement && ReactDOM.createPortal(children, rootElement);
+};
