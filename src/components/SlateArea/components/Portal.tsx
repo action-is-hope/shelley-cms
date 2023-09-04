dc46f3c23454d688d7832978912e73de822f43dd
Tamas Balint <balint.tamas87@gmail.com>
2023-09-04 13:45:37 +0100

    Revert "Fix: Portal types"
    This reverts commit 8c590323aa28776b56b1b7a28ba12612a7a86af1.

--- src/components/SlateArea/components/Portal.tsx
+++ src/components/SlateArea/components/Portal.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from "react-dom";
 
-export const Portal = ({ children }: Element) => {
-  const rootElement = window.document.getElementById("root");
-  rootElement && ReactDOM.createPortal(children, rootElement);
-};
+export const Portal = ({ children }: any) =>
+  ReactDOM.createPortal(children, window.document.getElementById("root")!);
